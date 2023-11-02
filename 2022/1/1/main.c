1541d520f121ef003dda1100f512560a43d26d02
sennahhh <hannes@bolmehag.se>
2023-11-02 15:37:37 +0100

    day1

---
+++ 2022/1/1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(void)
+{
+    FILE* fp;
+    int temp_cals = 0;
+    int most_cals = 0;
+    char buf[1024];
+
+    fp = fopen("../input1.txt", "r");
+
+    while(fgets(buf, sizeof(buf), fp))
+    {
+        if(strcmp(buf, "\n") != 0)
+        {
+            temp_cals += atoi(buf);
+        } else {
+            if(most_cals < temp_cals) 
+            {
+                most_cals = temp_cals;
+            }
+            temp_cals = 0;
+        }
+    }
+    fclose(fp);
+    printf("%d\n", most_cals);
+
+    return 0;
+}
---
+++ 2022/1/2/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(void)
+{
+    FILE* fp;
+    int cals = 0;
+    int top_3_cals[] = {0, 0, 0};
+    char buf[1024];
+
+    fp = fopen("../input1.txt", "r");
+
+    while(fgets(buf, sizeof(buf), fp))
+    {
+        if(strcmp(buf, "\n") != 0)
+        {
+            cals += atoi(buf);
+        } else {
+            int temp1 = 0;
+            int temp2 = 0;
+            for(int i = 0; i < 2; i++)
+            {
+                if(cals > top_3_cals[i])
+                {
+                    temp1 = top_3_cals[i];
+                    top_3_cals[i] = cals;
+                    if(i == 2)
+                    {
+                        temp2 = top_3_cals[1];
+                        top_3_cals[1] = temp1;
+                        top_3_cals[0] = temp2;
+                    } else if(i == 1) {
+                        top_3_cals[0] = temp1;
+                    }
+                }
+            }
+            cals = 0;
+        }
+    }
+    fclose(fp);
+    printf("%d\n", top_3_cals[0] + top_3_cals[1] + top_3_cals[2]);
+    return 0;
+}
