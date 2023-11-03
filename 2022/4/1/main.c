1fe04597489e0e0af4a9d9ce2bc14c8188551523
sennahhh <hannes@bolmehag.se>
2023-11-03 11:20:37 +0100

    day4

---
+++ 2022/4/1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+    FILE* fp;
+    char buf[1024];
+
+    while(fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        
+    }
+
+    return 0;
+}
