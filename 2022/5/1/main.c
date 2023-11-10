84498961b261e28079940eb24fe23ff95d57fb7f
sennahhh <hannes@bolmehag.se>
2023-11-10 16:31:11 +0100

    day5

--- 2022/5/1/main.c
+++ 2022/5/1/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int get_int_from_char(char c) {
+    return c - 48;
+}
 
 int main(void) {
+    FILE* fp;
+    fp = fopen("../input.txt", "r");
+    char* stacks[9];
+
+
 
     return 0;
 }
