813ad07de616d1a9d1d23f3089bc79aa57024d11
THUMBC_UP <hannes@bolmehag.se>
2023-12-03 22:26:43 +0100

    23 day1

--- 2023/1/2.c
+++ 2023/1/2.c
@@ -2,37 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    char* name;
+    int value;
+} number;
+
+typedef struct {
+    char* p;
+    int value;
+} pointerarr_object;
+
 int main(void) {
     FILE *fp;
     fp = fopen("./input.txt", "r");
     char buf[1024];
-    char** num_words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
+    number numbers[10] = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
     int tot = 0;
 
     while(fgets(buf, sizeof(buf), fp) != NULL) {
+        pointerarr_object num_pointer_arr[20];
+        size_t len = strlen(buf);
+        int j = 0;
+        int x = 0;
         int num1 = -1;
         int num2 = -1;
-        int num;
-        int position = 0;
-        int len = strlen(buf);
-        for(int i = 0; i < len; i++) {
+        int num = 0;
+        
+       //number words
+       for(int i = 0; i < 10; i++) {
+            char* ret = NULL;
+            char* temp_buf = buf;
+            while((ret = strstr(temp_buf, numbers[i].name)) != NULL) {
+                num_pointer_arr[j].p = ret;
+                num_pointer_arr[j].value = numbers[i].value;
+                temp_buf = ret + 1;
+                j++;
+            }
+       }
+
+        //number values
+       for(int i = 0; i < len; i++) {
             if(buf[i] < 58 && buf[i] > 47) {
-                if(position == 0) {
-                    num1 = buf[i] - 48;
-                    position++;
-                } else {
-                    num2 = buf[i] - 48;
+                num_pointer_arr[j].p = &buf[i];
+                num_pointer_arr[j].value = buf[i] - 48;
+                j++;       
+            }
+       }
+
+        //value
+        for(int i = 0; i < len; i++) {
+            for(int y = 0; y <= j; y++) {
+                if(&buf[i] == num_pointer_arr[y].p) {
+                    if(x < 1) {
+                        num1 = num_pointer_arr[y].value;
+                        x++;
+                    } else {
+                        num2 = num_pointer_arr[y].value;
+                    }
+                    
                 }
-            } 
-        }
+            }
+       }
 
         if(num2 == -1) {
             num = 10 * num1 + num1;
         } else {
             num = 10 * num1 + num2;
         }
+        // printf("%s", buf);
+        // printf("%d\n", num);
+        tot += num;     
         
-        tot += num;
     }
 
     printf("%d\n", tot);
